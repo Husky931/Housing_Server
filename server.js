8382c9ec0f1747186bd1fec8cd409fe1425103ba
Xz33M Hask <pecevgligor@protonmail.com>
2022-01-16 13:12:54 +0100

    basic routes

--- server.js
+++ server.js
@@ -8,9 +8,67 @@ console.log(process.env.NODE_ENV)
 const app = express()
 
 app.get("/", (req, res) => {
-    console.log(req.method)
-    res.send("hi hao")
+    res.sendStatus(200)
 })
+
+app.get("/listings", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Receive array of all apartment",
+    })
+})
+
+app.get("/listings", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Receive array of all apartment",
+    })
+})
+app.get("/listings/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Receive this apartment",
+    })
+})
+
+app.post("/listings", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Create new apartment",
+    })
+})
+
+app.put("/listings/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        number: req.params.id,
+        data: "Update apartment",
+    })
+})
+
+app.delete("/listings/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        number: req.params.id,
+        data: "Delete this apartment",
+    })
+})
+
+app.post("/login", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Login user",
+    })
+})
+app.post("/register", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Register user",
+    })
+})
+
+app.post("/register")
+
 const PORT = 5000
 
 app.listen(PORT, () => console.log(`SERVER IS RUNNING IN ${process.env.NODE_ENV} mode on port ${PORT}`))
