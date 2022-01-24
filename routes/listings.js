f7b1d60ce70e6c938edf781c0d8137faf31af5f7
Xz33M Hask <pecevgligor@protonmail.com>
2022-01-24 12:14:12 +0100

    first commit

--- routes/listings.js
+++ routes/listings.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
     res.status(200).json({
         success: true,
-        data: "Receive this apartment",
+        data: req.params.id,
     })
 })
 
--- server.js
+++ server.js
@@ -1,61 +1,34 @@
 const express = require("express")
 const dotenv = require("dotenv")
-const mongoose = require("mongoose")
-
-const User = require("./model/user")
 
 const listings = require("./routes/listings")
 
 dotenv.config()
 
-mongoose
-    .connect("mongodb://localhost:27017/test")
-    .then(() => {
-        const app = express()
+const app = express()
 
-        app.use("/listings", listings)
+app.use("/listings", listings)
 
-        app.get("/", (req, res) => {
-            res.sendStatus(200)
-        })
-
-        app.get("/test", async (req, res) => {
-            const user = await User.find()
-            res.send({ data: user })
-        })
-
-        app.post("/login", (req, res) => {
-            console.log(req.body)
-            res.status(200).json({
-                success: true,
-                data: "Login user",
-            })
-        })
-        app.post("/register", (req, res) => {
-            const fname = req.body.fname
-            const lname = req.body.lname
-            const email = req.body.email
-            const password = req.body.password
-
-            let registerUser = new User({
-                fname,
-                lname,
-                email,
-                password,
-            })
-            registerUser.save().then((data) => {
-                res.send(data).catch((err) => {
-                    console.log(err)
-                })
-            })
-        })
-
-        app.post("/register")
-
-        const PORT = 5000
-
-        app.listen(PORT, () => console.log(`SERVER IS RUNNING IN ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Homepage ok",
     })
-    .catch(() => {
-        console.log("database connection failed")
+})
+
+app.post("/login", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Login user",
     })
+})
+app.post("/register", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Login user",
+    })
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`SERVER IS RUNNING IN ${process.env.NODE_ENV} mode on port ${PORT}`))
